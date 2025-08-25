35d2ff79822a5f5a8a23698486d4af2028a674d5
John Krebian <sirchanmp@hacari.net>
2025-08-25 20:07:50 +0630

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/main.css";
 import { SpeedInsights } from '@vercel/speed-insights/next;
+import { Analytics } from "@vercel/analytics/next";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
